68d55d35c7c9fabbce0f9cd57839f9409e2b23dd
Artter <kasinat3mjt@gmail.com>
2025-04-04 18:45:22 +0700

    try Ecpress Router

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { assignments as assignmentsFromFile } from "./data/assignments.js";
 import { comments as commentsFromFile } from "./data/comments.js";
-
+import accountRouter from "./router/accounts.mjs";
 let assignments = [...assignmentsFromFile];
 let comments = [...commentsFromFile];
 
@@ -10,6 +10,7 @@ const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
+app.use("/accounts",accountRouter);
 
 app.get("/", (req, res) => {
   return res.send("Hello Teacher!!");
@@ -147,25 +148,6 @@ app.delete("/assignments/:id/comments", (req, res) => {
   });
 });
 
-app.get("/accounts", function (req, res) {
-  res.send("View all accounts");
-});
-
-app.get("/accounts/:id", function (req, res) {
-  res.send("View an account by id");
-});
-
-app.post("/accounts", function (req, res) {
-  res.send("Create an account");
-});
-
-app.put("/accounts/:id", function (req, res) {
-  res.send("Update an account by id");
-});
-
-app.delete("/accounts/:id", function (req, res) {
-  res.send("Delete an account by id");
-});
 
 app.listen(port, () => {
   console.log(`Server is running at the port ${port}`);
